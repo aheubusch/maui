32f859ee04785bb501337293df4a9f4441d12a12
Eilon Lipton <Eilon@users.noreply.github.com>
2024-09-26 13:53:32 -0700

    More timeout

--- src/TestUtils/src/Microsoft.Maui.IntegrationTests/Utilities/DotnetInternal.cs
+++ src/TestUtils/src/Microsoft.Maui.IntegrationTests/Utilities/DotnetInternal.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Maui.IntegrationTests
 	{
 		static readonly string DotnetRoot = Path.Combine(TestEnvironment.GetMauiDirectory(), "bin", "dotnet");
 		static readonly string DotnetTool = Path.Combine(DotnetRoot, "dotnet");
-		const int DEFAULT_TIMEOUT = 900;
+		const int DEFAULT_TIMEOUT = 30 * 60; // 30 minutes
 
 		private static string ConstructBuildArgs(string projectFile, string config, string target = "", string framework = "", IEnumerable<string>? properties = null, string binlogPath = "", string runtimeIdentifier = "", bool isPublishing = false)
 		{
